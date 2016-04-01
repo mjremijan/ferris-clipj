300c0ac51b848f6b30e12c20f9e2a088bb3a3378
Michael Remijan <mjremijan@yahoo.com>
2016-04-01 14:34:46 -0500

    Inject all the UI parts into TrayView and then put it together.

--- src/main/java/org/ferris/clipj/window/tray/TrayView.java
+++ src/main/java/org/ferris/clipj/window/tray/TrayView.java
@@ -1,11 +1,15 @@
 package org.ferris.clipj.window.tray;
 
 import java.awt.AWTException;
+import java.awt.Image;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import org.apache.log4j.Logger;
+import org.ferris.clipj.window.about.About;
+import org.ferris.clipj.window.image.qualifier.TrayImage;
 
 /**
  *
@@ -16,16 +20,33 @@ public class TrayView {
 
     @Inject
     protected Logger log;
-    
+
     @Inject
+    protected About about;
+
+    @Inject
+    @TrayImage
+    protected Image image;
+
+    @Inject
+    protected TrayPopupMenu popupMenu;
+
     protected TrayIcon trayIcon;
-    
+
+    @PostConstruct
+    public void putTheUiTogether() {
+        trayIcon = new TrayIcon(image);
+        trayIcon.setToolTip(String.format("Ferris ClipJ (%s)", about.getVersion()));
+        trayIcon.setPopupMenu(popupMenu);
+        //trayIcon.addActionListener(e -> System.out.println("clicked on clipj"));
+    }
+
     public void startup() {
         log.info("ENTER");
-        
-        SystemTray tray 
+
+        SystemTray tray
             = SystemTray.getSystemTray();
-        
+
         try {
             tray.add(trayIcon);
         } catch (AWTException e) {
@@ -33,11 +54,4 @@ public class TrayView {
         }
     }
 
-//    @PostConstruct
-//    public void putTheUiTogether() {
-//        trayIcon.setToolTip("ClipJ");
-//        trayIcon.setPopupMenu(new ClipboardHistoryMenu())));
-//        
-//        trayIcon.addActionListener(e -> System.out.println("clicked on clipj"));
-//        return myTrayIcon;
 }
